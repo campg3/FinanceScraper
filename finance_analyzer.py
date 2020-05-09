c994dbf9f7d0979de240bfca10da07a09f5b0100
Garrett Campbell <campg3@vt.edu>
2020-05-09 15:06:36 -0400

    Added summary of 1 month ago, 6 months ago, 1 year ago

--- finance_analyzer.py
+++ finance_analyzer.py
@@ -7,27 +7,95 @@ from datetime import date
 from xlutils.copy import copy
 import openpyxl as op
 
-def setUpCurrentAndNew():
+def setUpNew():
     newSheet['A1'] = "Bank Account"
-    currentSheet['A1'] = "Bank Account"
     newSheet['A2'] = "Checking"
-    currentSheet['A2'] = "Checking"
     newSheet['A3'] = "Savings"
-    currentSheet['A3'] = "Savings"
     newSheet['A4'] = "Money Market"
-    currentSheet['A4'] = "Money Market"
     newSheet['A5'] = "Total"
-    currentSheet['A5'] = "Total"
+
+def setUpSummary():
+    summarySheet['A1'] = "Current Bank Account"
+    summarySheet['A2'] = "Checking"
+    summarySheet['A3'] = "Savings"
+    summarySheet['A4'] = "Money Market"
+    summarySheet['A5'] = "Total"
+    summarySheet['C1'] = "Delta in 1 month"
+    summarySheet['E1'] = "Delta in 6 months"
+    summarySheet['G1'] = "Delta in 1 year"
 
 def allocateBankAccountValues():
     newSheet['B2'] = account.checking
-    currentSheet['B2'] = account.checking
+    summarySheet['B2'] = account.checking
     newSheet['B3'] = account.saving
-    currentSheet['B3'] = account.saving
+    summarySheet['B3'] = account.saving
     newSheet['B4'] = account.mm
-    currentSheet['B4'] = account.mm
+    summarySheet['B4'] = account.mm
     newSheet['B5'] = account.sumAccount()
-    currentSheet['B5'] = account.sumAccount()
+    summarySheet['B5'] = account.sumAccount()
+
+def allocateSummaryBankAccount():
+    sheetList = writeWB.sheetnames
+    length = len(sheetList)
+    if (length <= 2):
+        summarySheet['C2'] = "N/A"
+        summarySheet['C3'] = "N/A"
+        summarySheet['C4'] = "N/A"
+        summarySheet['C5'] = "N/A"
+        summarySheet['E2'] = "N/A"
+        summarySheet['E3'] = "N/A"
+        summarySheet['E4'] = "N/A"
+        summarySheet['E5'] = "N/A"
+        summarySheet['G2'] = "N/A"
+        summarySheet['G3'] = "N/A"
+        summarySheet['G4'] = "N/A"
+        summarySheet['G5'] = "N/A"
+    elif (length <= 6):
+        oneMonthAgo = writeWB[sheetList[length-2]]
+        summarySheet['C2'] = account.checking - oneMonthAgo['B2'].value
+        summarySheet['C3'] = account.saving - oneMonthAgo['B3'].value
+        summarySheet['C4'] = account.mm - oneMonthAgo['B4'].value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - oneMonthAgo['B5'].value
+        summarySheet['E2'] = "N/A"
+        summarySheet['E3'] = "N/A"
+        summarySheet['E4'] = "N/A"
+        summarySheet['E5'] = "N/A"
+        summarySheet['G2'] = "N/A"
+        summarySheet['G3'] = "N/A"
+        summarySheet['G4'] = "N/A"
+        summarySheet['G5'] = "N/A"
+    elif (length <= 12):
+        oneMonthAgo = writeWB[sheetList[length-2]]
+        sixMonthAgo = writeWB[sheetList[length-7]]
+        summarySheet['C2'] = account.checking - oneMonthAgo['B2'].value
+        summarySheet['C3'] = account.saving - oneMonthAgo['B3'].value
+        summarySheet['C4'] = account.mm - oneMonthAgo['B4'].value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - oneMonthAgo['B5'].value
+        summarySheet['E2'] = account.checking - sixMonthAgo['B2'].value
+        summarySheet['E3'] = account.saving - sixMonthAgo['B3'].value
+        summarySheet['E4'] = account.mm - sixMonthAgo['B4'].value
+        summarySheet['E5'] = round(account.sumAccount(), 2) - sixMonthAgo['B5'].value
+        summarySheet['G2'] = "N/A"
+        summarySheet['G3'] = "N/A"
+        summarySheet['G4'] = "N/A"
+        summarySheet['G5'] = "N/A"
+    else:
+        oneMonthAgo = writeWB[sheetList[length-2]]
+        sixMonthAgo = writeWB[sheetList[length-7]]
+        oneYearAgo = writeWB[sheetList[length-13]]
+        print(oneYearAgo['B5'].value)
+        summarySheet['C2'] = account.checking - oneMonthAgo['B2'].value
+        summarySheet['C3'] = account.saving - oneMonthAgo['B3'].value
+        summarySheet['C4'] = account.mm - oneMonthAgo['B4'].value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - oneMonthAgo['B5'].value
+        summarySheet['E2'] = account.checking - sixMonthAgo['B2'].value
+        summarySheet['E3'] = account.saving - sixMonthAgo['B3'].value
+        summarySheet['E4'] = account.mm - sixMonthAgo['B4'].value
+        summarySheet['E5'] = round(account.sumAccount(), 2) - sixMonthAgo['B5'].value
+        summarySheet['G2'] = account.checking - oneYearAgo['B2'].value
+        summarySheet['G3'] = account.saving - oneYearAgo['B3'].value
+        summarySheet['G4'] = account.mm - oneYearAgo['B4'].value
+        summarySheet['G5'] = round(account.sumAccount(), 2) - oneYearAgo['B5'].value
 
 # Give the location of the file
 loc = ("test.xlsx")
@@ -50,15 +118,18 @@ d1 = today.strftime("%d_%m_%Y")
 writeWB = op.load_workbook("FinanceSummary.xlsx")
 try:
     possibleDelete = writeWB["Sheet 1"]
-    writeWB.remove_sheet(possibleDelete)
+    writeWB.remove(writeWB["Sheet 1"])
+    summarySheet = writeWB.create_sheet("Summary")
+    setUpSummary()
 except:
     print("No 'Sheet 1', no worries")
 
 newSheet = writeWB.create_sheet(d1)
-currentSheet = writeWB["Current"]
+summarySheet = writeWB["Summary"]
 
-setUpCurrentAndNew()
+setUpNew()
 allocateBankAccountValues()
+allocateSummaryBankAccount()
 
 writeWB.save("FinanceSummary.xlsx")
 
