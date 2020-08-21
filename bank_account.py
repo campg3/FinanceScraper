ce531d1466f8cf9010d7daa4aa9d7f0498dad29c
Garrett Campbell <campg3@vt.edu>
2020-08-21 09:32:25 -0400

    bug fix

--- bank_account.py
+++ bank_account.py
@@ -101,7 +101,6 @@ def allocateBankAccountValues(summarySheet, archiveSheet, account):
 # fills out the summary page
 def allocateSummaryBankAccount(summarySheet, archiveSheet, account):
     numEntries = archiveSheet.max_row
-
     # no analysis available
     if (numEntries-1 == 1):
         summarySheet['C2'] = "N/A"
@@ -126,10 +125,10 @@ def allocateSummaryBankAccount(summarySheet, archiveSheet, account):
         summarySheet['G5'] = "N/A"
     # analysis on 1 month available
     elif (numEntries-1 < 4):
-        summarySheet['C2'] = account.checking - archiveSheet.cell(row=numEntries-1, column=2).value
-        summarySheet['C3'] = account.saving - archiveSheet.cell(row=numEntries-1, column=3).value
-        summarySheet['C4'] = account.mm - archiveSheet.cell(row=numEntries-1, column=4).value
-        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-1, column=5).value
+        summarySheet['C2'] = account.checking - archiveSheet.cell(row=3, column=2).value
+        summarySheet['C3'] = account.saving - archiveSheet.cell(row=3, column=3).value
+        summarySheet['C4'] = account.mm - archiveSheet.cell(row=3, column=4).value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=3, column=5).value
         summarySheet['D2'] = "N/A"
         summarySheet['D3'] = "N/A"
         summarySheet['D4'] = "N/A"
@@ -148,14 +147,14 @@ def allocateSummaryBankAccount(summarySheet, archiveSheet, account):
         summarySheet['G5'] = "N/A"
     # analysis on 1, 3 month
     elif (numEntries-1 < 7):
-        summarySheet['C2'] = account.checking - archiveSheet.cell(row=numEntries-1, column=2).value
-        summarySheet['C3'] = account.saving - archiveSheet.cell(row=numEntries-1, column=3).value
-        summarySheet['C4'] = account.mm - archiveSheet.cell(row=numEntries-1, column=4).value
-        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-1, column=5).value
-        summarySheet['D2'] = account.checking - archiveSheet.cell(row=numEntries-3, column=2).value
-        summarySheet['D3'] = account.saving - archiveSheet.cell(row=numEntries-3, column=3).value
-        summarySheet['D4'] = account.mm - archiveSheet.cell(row=numEntries-3, column=4).value
-        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-3, column=5).value
+        summarySheet['C2'] = account.checking - archiveSheet.cell(row=3, column=2).value
+        summarySheet['C3'] = account.saving - archiveSheet.cell(row=3, column=3).value
+        summarySheet['C4'] = account.mm - archiveSheet.cell(row=3, column=4).value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=3, column=5).value
+        summarySheet['D2'] = account.checking - archiveSheet.cell(row=5, column=2).value
+        summarySheet['D3'] = account.saving - archiveSheet.cell(row=5, column=3).value
+        summarySheet['D4'] = account.mm - archiveSheet.cell(row=5, column=4).value
+        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=5, column=5).value
         summarySheet['E2'] = "N/A"
         summarySheet['E3'] = "N/A"
         summarySheet['E4'] = "N/A"
@@ -170,18 +169,18 @@ def allocateSummaryBankAccount(summarySheet, archiveSheet, account):
         summarySheet['G5'] = "N/A"
     # analysis on 1, 3, 6 month
     elif (numEntries-1 < 10):
-        summarySheet['C2'] = account.checking - archiveSheet.cell(row=numEntries-1, column=2).value
-        summarySheet['C3'] = account.saving - archiveSheet.cell(row=numEntries-1, column=3).value
-        summarySheet['C4'] = account.mm - archiveSheet.cell(row=numEntries-1, column=4).value
-        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-1, column=5).value
-        summarySheet['D2'] = account.checking - archiveSheet.cell(row=numEntries-3, column=2).value
-        summarySheet['D3'] = account.saving - archiveSheet.cell(row=numEntries-3, column=3).value
-        summarySheet['D4'] = account.mm - archiveSheet.cell(row=numEntries-3, column=4).value
-        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-3, column=5).value
-        summarySheet['E2'] = account.checking - archiveSheet.cell(row=numEntries-6, column=2).value
-        summarySheet['E3'] = account.saving - archiveSheet.cell(row=numEntries-6, column=3).value
-        summarySheet['E4'] = account.mm - archiveSheet.cell(row=numEntries-6, column=4).value
-        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-6, column=5).value
+        summarySheet['C2'] = account.checking - archiveSheet.cell(row=3, column=2).value
+        summarySheet['C3'] = account.saving - archiveSheet.cell(row=3, column=3).value
+        summarySheet['C4'] = account.mm - archiveSheet.cell(row=3, column=4).value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=3, column=5).value
+        summarySheet['D2'] = account.checking - archiveSheet.cell(row=5, column=2).value
+        summarySheet['D3'] = account.saving - archiveSheet.cell(row=5, column=3).value
+        summarySheet['D4'] = account.mm - archiveSheet.cell(row=5, column=4).value
+        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=5, column=5).value
+        summarySheet['E2'] = account.checking - archiveSheet.cell(row=8, column=2).value
+        summarySheet['E3'] = account.saving - archiveSheet.cell(row=8, column=3).value
+        summarySheet['E4'] = account.mm - archiveSheet.cell(row=8, column=4).value
+        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=8, column=5).value
         summarySheet['F2'] = "N/A"
         summarySheet['F3'] = "N/A"
         summarySheet['F4'] = "N/A"
@@ -192,46 +191,46 @@ def allocateSummaryBankAccount(summarySheet, archiveSheet, account):
         summarySheet['G5'] = "N/A"
     # analysis on 1, 3, 6, 9 month
     elif (numEntries-1 < 13):
-        summarySheet['C2'] = account.checking - archiveSheet.cell(row=numEntries-1, column=2).value
-        summarySheet['C3'] = account.saving - archiveSheet.cell(row=numEntries-1, column=3).value
-        summarySheet['C4'] = account.mm - archiveSheet.cell(row=numEntries-1, column=4).value
-        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-1, column=5).value
-        summarySheet['D2'] = account.checking - archiveSheet.cell(row=numEntries-3, column=2).value
-        summarySheet['D3'] = account.saving - archiveSheet.cell(row=numEntries-3, column=3).value
-        summarySheet['D4'] = account.mm - archiveSheet.cell(row=numEntries-3, column=4).value
-        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-3, column=5).value
-        summarySheet['E2'] = account.checking - archiveSheet.cell(row=numEntries-6, column=2).value
-        summarySheet['E3'] = account.saving - archiveSheet.cell(row=numEntries-6, column=3).value
-        summarySheet['E4'] = account.mm - archiveSheet.cell(row=numEntries-6, column=4).value
-        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-6, column=5).value
-        summarySheet['F2'] = account.checking - archiveSheet.cell(row=numEntries-9, column=2).value
-        summarySheet['F3'] = account.saving - archiveSheet.cell(row=numEntries-9, column=3).value
-        summarySheet['F4'] = account.mm - archiveSheet.cell(row=numEntries-9, column=4).value
-        summarySheet['F5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-9, column=5).value
+        summarySheet['C2'] = account.checking - archiveSheet.cell(row=3, column=2).value
+        summarySheet['C3'] = account.saving - archiveSheet.cell(row=3, column=3).value
+        summarySheet['C4'] = account.mm - archiveSheet.cell(row=3, column=4).value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=3, column=5).value
+        summarySheet['D2'] = account.checking - archiveSheet.cell(row=5, column=2).value
+        summarySheet['D3'] = account.saving - archiveSheet.cell(row=5, column=3).value
+        summarySheet['D4'] = account.mm - archiveSheet.cell(row=5, column=4).value
+        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=5, column=5).value
+        summarySheet['E2'] = account.checking - archiveSheet.cell(row=8, column=2).value
+        summarySheet['E3'] = account.saving - archiveSheet.cell(row=8, column=3).value
+        summarySheet['E4'] = account.mm - archiveSheet.cell(row=8, column=4).value
+        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=8, column=5).value
+        summarySheet['F2'] = account.checking - archiveSheet.cell(row=11, column=2).value
+        summarySheet['F3'] = account.saving - archiveSheet.cell(row=11, column=3).value
+        summarySheet['F4'] = account.mm - archiveSheet.cell(row=11, column=4).value
+        summarySheet['F5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=11, column=5).value
         summarySheet['G2'] = "N/A"
         summarySheet['G3'] = "N/A"
         summarySheet['G4'] = "N/A"
         summarySheet['G5'] = "N/A"
     # analysis on 1, 3, 6, 9 month, 1 year
     else:
-        summarySheet['C2'] = account.checking - archiveSheet.cell(row=numEntries-1, column=2).value
-        summarySheet['C3'] = account.saving - archiveSheet.cell(row=numEntries-1, column=3).value
-        summarySheet['C4'] = account.mm - archiveSheet.cell(row=numEntries-1, column=4).value
-        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-1, column=5).value
-        summarySheet['D2'] = account.checking - archiveSheet.cell(row=numEntries-3, column=2).value
-        summarySheet['D3'] = account.saving - archiveSheet.cell(row=numEntries-3, column=3).value
-        summarySheet['D4'] = account.mm - archiveSheet.cell(row=numEntries-3, column=4).value
-        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-3, column=5).value
-        summarySheet['E2'] = account.checking - archiveSheet.cell(row=numEntries-6, column=2).value
-        summarySheet['E3'] = account.saving - archiveSheet.cell(row=numEntries-6, column=3).value
-        summarySheet['E4'] = account.mm - archiveSheet.cell(row=numEntries-6, column=4).value
-        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-6, column=5).value
-        summarySheet['F2'] = account.checking - archiveSheet.cell(row=numEntries-9, column=2).value
-        summarySheet['F3'] = account.saving - archiveSheet.cell(row=numEntries-9, column=3).value
-        summarySheet['F4'] = account.mm - archiveSheet.cell(row=numEntries-9, column=4).value
-        summarySheet['F5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-9, column=5).value
-        summarySheet['G2'] = account.checking - archiveSheet.cell(row=numEntries-12, column=2).value
-        summarySheet['G3'] = account.saving - archiveSheet.cell(row=numEntries-12, column=3).value
-        summarySheet['G4'] = account.mm - archiveSheet.cell(row=numEntries-12, column=4).value
-        summarySheet['G5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=numEntries-12, column=5).value
+        summarySheet['C2'] = account.checking - archiveSheet.cell(row=3, column=2).value
+        summarySheet['C3'] = account.saving - archiveSheet.cell(row=3, column=3).value
+        summarySheet['C4'] = account.mm - archiveSheet.cell(row=3, column=4).value
+        summarySheet['C5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=3, column=5).value
+        summarySheet['D2'] = account.checking - archiveSheet.cell(row=5, column=2).value
+        summarySheet['D3'] = account.saving - archiveSheet.cell(row=5, column=3).value
+        summarySheet['D4'] = account.mm - archiveSheet.cell(row=5, column=4).value
+        summarySheet['D5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=5, column=5).value
+        summarySheet['E2'] = account.checking - archiveSheet.cell(row=8, column=2).value
+        summarySheet['E3'] = account.saving - archiveSheet.cell(row=8, column=3).value
+        summarySheet['E4'] = account.mm - archiveSheet.cell(row=8, column=4).value
+        summarySheet['E5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=8, column=5).value
+        summarySheet['F2'] = account.checking - archiveSheet.cell(row=11, column=2).value
+        summarySheet['F3'] = account.saving - archiveSheet.cell(row=11, column=3).value
+        summarySheet['F4'] = account.mm - archiveSheet.cell(row=11, column=4).value
+        summarySheet['F5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=11, column=5).value
+        summarySheet['G2'] = account.checking - archiveSheet.cell(row=14, column=2).value
+        summarySheet['G3'] = account.saving - archiveSheet.cell(row=14, column=3).value
+        summarySheet['G4'] = account.mm - archiveSheet.cell(row=14, column=4).value
+        summarySheet['G5'] = round(account.sumAccount(), 2) - archiveSheet.cell(row=14, column=5).value
 
